09ee14c941830990004eb9fa0c8cf5fa5a77bef8
arturmalinowski <artur@wow.com>
2014-10-20 20:44:00 +0100

    Artur: added anagram test

--- edmund-core/src/test/java/cs/group/edmund/clue/AnagramClueTest.java
+++ edmund-core/src/test/java/cs/group/edmund/clue/AnagramClueTest.java
@@ -3,8 +3,10 @@ package cs.group.edmund.clue;
 import org.junit.Ignore;
 import org.junit.Test;
 
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.CoreMatchers.containsString;
 
 public class AnagramClueTest {
 
@@ -17,4 +19,11 @@ public class AnagramClueTest {
         assertThat(solvedWord, is("nights"));
     }
 
+    @Ignore
+    @Test
+    public void anagramClueTestCanBeCreated() {
+        Clue clue = new AnagramClue();
+        String crypticCrossword = clue.create("nights");
+        assertThat(crypticCrossword, containsString("things"));
+    }
 }
