6d94eefaa23a961864b6979d7fe297ea285b96d4
petrie7 <pt211@kent.ac.uk>
2014-11-24 14:18:25 +0000

    Pete : increased double definition clue solver effectiveness.

--- edmund-core/src/main/java/cs/group/edmund/clue/DoubleDefinitionsClue.java
+++ edmund-core/src/main/java/cs/group/edmund/clue/DoubleDefinitionsClue.java
@@ -9,9 +9,13 @@ public class DoubleDefinitionsClue implements Clue {
 
     private ArrayList<String> firstElementList = new ArrayList<>();
     private ArrayList<String> secondElementList = new ArrayList<>();
+    private ArrayList<String> leftBackupWordList = new ArrayList<>();
+    private ArrayList<String> rightBackupWordList = new ArrayList<>();
     private boolean matchingWordFound;
     private String answer;
     private ArrayList<Integer> answerLength = new ArrayList<>();
+    private String leftBackupWord;
+    private String rightBackupWord;
 
     @Override
     public String create(String word) {
@@ -20,7 +24,6 @@ public class DoubleDefinitionsClue implements Clue {
 
     @Override
     public boolean isRelevant(String phrase) {
-        answerLength.add(0);
         return findMatchingWords(phrase);
     }
 
@@ -43,6 +46,8 @@ public class DoubleDefinitionsClue implements Clue {
     public Boolean findMatchingWords(String phrase) {
         String[] splitPhrase = phrase.split(" ");
         if (splitPhrase.length > 2) {
+            leftBackupWord = splitPhrase[1];
+            rightBackupWord = splitPhrase[splitPhrase.length - 2];
             splitPhrase = new String[]{splitPhrase[0], splitPhrase[splitPhrase.length - 1]};
         }
 
@@ -50,15 +55,15 @@ public class DoubleDefinitionsClue implements Clue {
 
         compareTheListsOfWords();
 
-        if (matchingWordFound) {
-            System.out.println("Answer found: " + answer);
-        }
-
         return matchingWordFound;
     }
 
     private void putRelatedWordsInTwoLists(String[] splitPhrase) {
         Thesaurus thesaurus = new Thesaurus();
+        Document documentLeftBackUp = thesaurus.getSynonymsAsDocument(leftBackupWord);
+        leftBackupWordList = thesaurus.addSynonymsToList(documentLeftBackUp);
+        Document documentRightBackUp = thesaurus.getSynonymsAsDocument(rightBackupWord);
+        rightBackupWordList = thesaurus.addSynonymsToList(documentRightBackUp);
         for (int i = 0; i < 2; i++) {
             Document document = thesaurus.getSynonymsAsDocument(splitPhrase[i]);
             if (i == 0 && document != null) {
@@ -72,12 +77,22 @@ public class DoubleDefinitionsClue implements Clue {
     private void compareTheListsOfWords() {
         matchingWordFound = false;
         for (String element : firstElementList) {
-            if (secondElementList.contains(element) && answerLength.get(0) == 0) {
+            if (secondElementList.contains(element) && answerLength.size() == 0) {
                 matchingWordFound = true;
                 answer = element;
             } else if (secondElementList.contains(element) && element.length() == answerLength.get(0)) {
                 matchingWordFound = true;
                 answer = element;
+            }
+            else if (rightBackupWordList.contains(element) && element.length() == answerLength.get(0)) {
+                matchingWordFound = true;
+                answer = element;
+            }
+        }
+        for (String secondListElement : secondElementList) {
+            if (leftBackupWordList.contains(secondListElement) && secondListElement.length() == answerLength.get(0)) {
+                matchingWordFound = true;
+                answer = secondListElement;
             }
         }
     }
--- edmund-core/src/test/java/cs/group/edmund/clue/DoubleDefinitionsClueTest.java
+++ edmund-core/src/test/java/cs/group/edmund/clue/DoubleDefinitionsClueTest.java
@@ -1,6 +1,7 @@
 package cs.group.edmund.clue;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -22,6 +23,11 @@ public class DoubleDefinitionsClueTest {
     }
 
     @Test
+    public void detectsClueIsNotADoubleDefinitionClue() {
+        assertEquals(clue.isRelevant("Bird allowed outside tavern"), false);
+    }
+
+    @Test
     public void limitedDoubleDefinitionClueCanBeSolved() {
         String solvedWord = clue.solve("Expensive sweet golden honey", 4);
 
@@ -29,8 +35,30 @@ public class DoubleDefinitionsClueTest {
     }
 
     @Test
+    public void doubleDefinitionClueWithClueWordNotAtTheLeftEndOfTheClueCanBeSolved() {
+        String solvedWord = clue.solve("Sweet expensive golden honey", 4);
+
+        assertThat(solvedWord, is("dear"));
+    }
+
+    @Test
+    public void doubleDefinitionClueWithClueWordNotAtTheRightEndOfTheClueCanBeSolved() {
+        String solvedWord = clue.solve("Expensive sweet honey golden", 4);
+
+        assertThat(solvedWord, is("dear"));
+    }
+
+    @Ignore
+    @Test
+    public void moreComplexDoubleDefinitionClueCanBeSolved() {
+        String solvedWord = clue.solve("A fast train, say", 7);
+
+        assertThat(solvedWord, is("Express"));
+    }
+
+    @Test
     public void handlesWhenNoMatchingWordIsFound() {
-        String solvedWord = clue.solve("Blah blah bluegh", 4);
+        String solvedWord = clue.solve("Bird allowed outside tavern", 4);
 
         assertThat(solvedWord, is("Answer not found :("));
     }
