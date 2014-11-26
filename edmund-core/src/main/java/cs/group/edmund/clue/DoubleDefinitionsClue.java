17b4f60085704aaae513738b92c4478839516fbc
petrie7 <pt211@kent.ac.uk>
2014-11-26 11:08:23 +0000

    Pete : enabled clue hints

--- edmund-core/src/main/java/cs/group/edmund/clue/DoubleDefinitionsClue.java
+++ edmund-core/src/main/java/cs/group/edmund/clue/DoubleDefinitionsClue.java
@@ -11,11 +11,13 @@ public class DoubleDefinitionsClue implements Clue {
     private List<String> secondElementList = new ArrayList<>();
     private List<String> leftBackupWordList = new ArrayList<>();
     private List<String> rightBackupWordList = new ArrayList<>();
-    private boolean matchingWordFound;
+    private List<Integer> answerLength = new ArrayList<>();
     private String answer;
-    private ArrayList<Integer> answerLength = new ArrayList<>();
     private String leftBackupWord;
     private String rightBackupWord;
+    private boolean matchingWordFound;
+    private int hintIndex = 0;
+    private char hintLetter = '.';
 
     @Override
     public String create(String word) {
@@ -32,7 +34,26 @@ public class DoubleDefinitionsClue implements Clue {
         for (int value : answerLength) {
             this.answerLength.add(value);
         }
-        return getAnswer(phrase);
+
+        extractHint(hint);
+
+        return getAnswer(phrase.toLowerCase());
+    }
+
+    private void extractHint(String hint) {
+        if (hint != null) {
+            // TODO enable this to work with answers of more than one word
+            if (hint.length() == answerLength.get(0)) {
+                for (int i = 0; i < hint.length(); i++) {
+                    if (hint.charAt(i) == '.') {
+                        hintIndex++;
+                    } else {
+                        hintLetter = hint.charAt(i);
+                        i = hint.length();
+                    }
+                }
+            }
+        }
     }
 
     public String getAnswer(String phrase) {
@@ -60,31 +81,45 @@ public class DoubleDefinitionsClue implements Clue {
 
     private void putRelatedWordsInTwoLists(String[] splitPhrase) {
         Thesaurus thesaurus = new Thesaurus();
+
+        // get Synonyms and related words for first word
+        firstElementList = thesaurus.getAllSynonymsXML(splitPhrase[0]);
+        firstElementList.addAll(thesaurus.getRelatedWordsXML(splitPhrase[0]));
+
+        // get Synonyms and related words for second word
+        secondElementList = thesaurus.getAllSynonymsXML(splitPhrase[1]);
+        secondElementList.addAll(thesaurus.getRelatedWordsXML(splitPhrase[1]));
+
         leftBackupWordList = thesaurus.getAllSynonymsXML(leftBackupWord);
+        leftBackupWordList.addAll(thesaurus.getRelatedWordsXML(leftBackupWord));
+
         rightBackupWordList = thesaurus.getAllSynonymsXML(rightBackupWord);
-            firstElementList = thesaurus.getAllSynonymsXML(splitPhrase[0]);
-            secondElementList = thesaurus.getAllSynonymsXML(splitPhrase[1]);
+        rightBackupWordList.addAll(thesaurus.getRelatedWordsXML(rightBackupWord));
     }
 
     private void compareTheListsOfWords() {
         matchingWordFound = false;
         for (String element : firstElementList) {
-            if (secondElementList.contains(element) && answerLength.size() == 0) {
+            if (secondElementList.contains(element) && answerLength.size() == 0 && matchesHint(element)) {
                 matchingWordFound = true;
                 answer = element;
-            } else if (secondElementList.contains(element) && element.length() == answerLength.get(0)) {
+            } else if (secondElementList.contains(element) && element.length() == answerLength.get(0) && matchesHint(element)) {
                 matchingWordFound = true;
                 answer = element;
-            } else if (rightBackupWordList.contains(element) && element.length() == answerLength.get(0)) {
+            } else if (rightBackupWordList.contains(element) && element.length() == answerLength.get(0) && matchesHint(element)) {
                 matchingWordFound = true;
                 answer = element;
             }
         }
         for (String secondListElement : secondElementList) {
-            if (leftBackupWordList.contains(secondListElement) && secondListElement.length() == answerLength.get(0)) {
+            if (leftBackupWordList.contains(secondListElement) && secondListElement.length() == answerLength.get(0) && matchesHint(secondListElement)) {
                 matchingWordFound = true;
                 answer = secondListElement;
             }
         }
+    }
+
+    public boolean matchesHint(String element) {
+        return hintLetter == '.' || element.charAt(hintIndex) == hintLetter;
     }
 }
