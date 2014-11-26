a6ce0bf2b36597dd14fb2ee0ff265d9916982ae9
petrie7 <pt211@kent.ac.uk>
2014-11-26 11:08:23 +0000

    Pete : enabled clue hints

--- edmund-core/src/test/java/cs/group/edmund/clue/DoubleDefinitionsClueTest.java
+++ edmund-core/src/test/java/cs/group/edmund/clue/DoubleDefinitionsClueTest.java
@@ -1,13 +1,18 @@
 package cs.group.edmund.clue;
 
+import com.googlecode.yatspec.junit.Row;
+import com.googlecode.yatspec.junit.Table;
+import com.googlecode.yatspec.junit.TableRunner;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
 
+@RunWith(TableRunner.class)
 public class DoubleDefinitionsClueTest {
 
     private DoubleDefinitionsClue clue;
@@ -27,33 +32,57 @@ public class DoubleDefinitionsClueTest {
         assertEquals(clue.isRelevant("Bird allowed outside tavern"), false);
     }
 
+    // Current success rate 42.8%, more examples needed
+    @Ignore
+    @Test
+    @Table({
+            @Row({"nothing for romance", ".o..", "4", "love"}),
+            @Row({"instant credit", "...k", "4", "tick"}),
+            @Row({"armor in the post", "m...", "4", "mail"}),
+            @Row({"sound warning for a temptress", "..r..", "5", "siren"}),
+            @Row({"fish detected by its odour", ".m...", "5", "smelt"}),
+            @Row({"run away from an infertile area", ".....t", "6", "desert"}),
+            @Row({"a fast train say", "e......", "7", "express"})
+    })
+    public void bulkClueTest(String cryticClue, String hint, String answerLength, String answer) {
+        String solvedWord = clue.solve(cryticClue, hint, Integer.parseInt(answerLength));
+
+        assertThat(solvedWord, is(answer));
+    }
+
+    @Test
+    public void correctWordIsFoundInListViaAHint() {
+        String solvedWord = clue.solve("Armor in the post", "m...", 4);
+
+        assertThat(solvedWord, is("mail"));
+    }
+
     @Test
     public void limitedDoubleDefinitionClueCanBeSolved() {
-        String solvedWord = clue.solve("Expensive sweet golden honey", null, 4);
+        String solvedWord = clue.solve("Expensive sweet golden honey", ".e..", 4);
 
         assertThat(solvedWord, is("dear"));
     }
 
     @Test
     public void doubleDefinitionClueWithClueWordNotAtTheLeftEndOfTheClueCanBeSolved() {
-        String solvedWord = clue.solve("Sweet expensive golden honey", null, 4);
+        String solvedWord = clue.solve("Sweet expensive golden honey", ".e..", 4);
 
         assertThat(solvedWord, is("dear"));
     }
 
     @Test
     public void doubleDefinitionClueWithClueWordNotAtTheRightEndOfTheClueCanBeSolved() {
-        String solvedWord = clue.solve("Expensive sweet honey golden", null, 4);
+        String solvedWord = clue.solve("Expensive sweet honey golden", ".e..", 4);
 
         assertThat(solvedWord, is("dear"));
     }
 
-    @Ignore
     @Test
     public void moreComplexDoubleDefinitionClueCanBeSolved() {
-        String solvedWord = clue.solve("A fast train, say", null, 7);
+        String solvedWord = clue.solve("A fast train, say", "e......", 7);
 
-        assertThat(solvedWord, is("Express"));
+        assertThat(solvedWord, is("express"));
     }
 
     @Test
@@ -65,7 +94,7 @@ public class DoubleDefinitionsClueTest {
 
     @Test
     public void correctLengthWordIsReturned() {
-        String solvedWord = clue.solve("Expensive expensive", null, 4);
+        String solvedWord = clue.solve("Expensive expensive", ".e..", 4);
         assertThat(solvedWord, is("dear"));
     }
 
