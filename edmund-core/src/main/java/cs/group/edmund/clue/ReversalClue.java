13738863688f8d403a9453c4a68cafb2d610e929
petrie7 <pt211@kent.ac.uk>
2014-12-06 14:34:54 +0000

    Pete : Very simple reversal clues can be solved

--- edmund-core/src/main/java/cs/group/edmund/clue/ReversalClue.java
+++ edmund-core/src/main/java/cs/group/edmund/clue/ReversalClue.java
@@ -8,6 +8,7 @@ public class ReversalClue implements Clue {
 
 
     private final List<String> keyWords;
+    private String answer = "Answer not found";
 
     public ReversalClue() {
         keyWords = asList("ABOUT", "AROUND", "ASCENDING", "BACK", "BACKED", "BACKING", "BACK-TO-FRONT", "BACKWARD", "BROUGHTABOUT", "BROUGHTUP", "CASTUP", "CLIMBING", "COMINGBACK", "COMINGUP", "COUNTER", "FLIPPED", "FLIPPING", "FROMTHEBOTTOM", "FROMTHEEAST", "FROMTHERIGHT", "FROMTHESOUTH", "GOINGBACK", "GOINGNORTH", "GOINGROUND", "GOINGUP", "GOINGWEST", "LIFTED", "LOOKINGBACK", "LOOKINGUP", "NORTHBOUND", "OVER", "OVERTURNED", "RAISED", "RAISING", "RETREAT", "RETREATING", "RETROGRADE", "RETROSPECTIVE", "REVERSED", "REVERSING", "REVOLUTIONARY", "RISING", "ROUND", "SENTBACK", "SENTUP", "SHOWNUP", "TAKENUP", "TURN", "TURNED", "TURNING", "TURNS", "UP", "UPENDED", "UPSET", "UPWARDLYMOBILE", "WESTBOUND", "WRITTENUP");
@@ -31,7 +32,17 @@ public class ReversalClue implements Clue {
 
     @Override
     public String solve(String phrase, String hint, int... answerLength) {
-        return null;
+        String[] words = phrase.split("\\s+");
+        for (int i = 0; i < words.length; i++) {
+            words[i] = words[i].replaceAll("[,?!]", "");
+        }
+
+        for (String word : words) {
+            answer = (word.length() == answerLength[0]) ? new StringBuilder(word).reverse().toString() : answer;
+        }
+
+        return answer;
     }
+
 
 }
