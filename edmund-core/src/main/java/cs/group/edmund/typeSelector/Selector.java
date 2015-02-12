e5d2f6955b646f5139f52c81fad6701a00300ad6
petrie7 <pt211@kent.ac.uk>
2015-02-12 11:24:59 +0000

    Pete : selector can now handle lists and deletion clue returns multiple answers in a list

--- edmund-core/src/main/java/cs/group/edmund/typeSelector/Selector.java
+++ edmund-core/src/main/java/cs/group/edmund/typeSelector/Selector.java
@@ -4,12 +4,15 @@ package cs.group.edmund.typeSelector;
 import cs.group.edmund.clue.*;
 import cs.group.edmund.utils.Thesaurus;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static java.util.Arrays.asList;
 
 public class Selector {
+
+    private List<List<String>> allPossibleAnswers = new ArrayList<>();
 
     public String retrieveAnswer(String phrase, String hint, int answerLength, Thesaurus thesaurus) throws Exception {
 
@@ -23,13 +26,29 @@ public class Selector {
         for (Clue clue : clues) {
             if (clue.isRelevant(phrase)) {
                 Optional<List<String>> answer = clue.solve(phrase, hint, answerLength);
-                if (answer.isPresent() && answer.get().size() == 1) {
-                    return answer.get().get(0);
+                if (answer.isPresent()) {
+                    allPossibleAnswers.add(answer.get());
                 }
             }
         }
-        throw new IllegalArgumentException("Clue could not be solved");
+        if (allPossibleAnswers.size() == 1) {
+            return allPossibleAnswers.get(0).get(0);
+        }
+        if (allPossibleAnswers.size() > 1) {
+            List<String> combinedAnswers = new ArrayList<>();
+            for (List<String> possibleAnswer : allPossibleAnswers) {
+                if (possibleAnswer.size() == 1) {
+                    return possibleAnswer.get(0);
+                } else {
+                    for (String answer : possibleAnswer) {
+                        combinedAnswers.add(answer);
+                    }
+                }
+            }
+            return combinedAnswers.toString();
+        }
 
+        throw new IllegalArgumentException("Clue could not be solved");
 
     }
 }
