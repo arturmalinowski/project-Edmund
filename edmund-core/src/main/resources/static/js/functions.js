13115b416f49ad66c2eecb74c460785da18054b4
Damian Van Vuuren <vanvuurendamian@gmail.com>
2015-02-19 16:47:22 +0000

    Damian: Added status pane to web UI

--- edmund-core/src/main/resources/static/js/functions.js
+++ edmund-core/src/main/resources/static/js/functions.js
@@ -1,5 +1,29 @@
+var clueArray = [];
+var answerArray = [];
+for (var i = 0; i < 15; i++) {
+	answerArray.push([".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."]);
+}
+
+setupTable(clueArray, answerArray);
+
 //
-function setupTable() {
+function runEdmund() {
+	// Tests to delete
+	//updateAnswerArray("0", "aaaaaa"); //
+	//updateAnswerArray("1", "5abcde"); //
+	//updateAnswerArray("2", "9abcdefg"); //
+	//updateAnswerArray("3", "10abcd"); //
+	//updateAnswerArray("12", "2abcd"); //
+	updateCrosswordByEdmund();
+	alert("Ran Edmund");
+}
+
+
+// Setup Table structure (COMPLETE)
+function setupTable(clueArray, answerArray) {
+	this.clueArray = clueArray;
+	this.answerArray = answerArray;
+
 	// Add rows and cells to table
 	var table = document.getElementById("crosswordTable");
 	for (var i = 0; i < 15; i++) {
@@ -18,27 +42,27 @@ function setupTable() {
 			setBlank(i, j);
 		}
 	}
+
+	// Setup rest of table
+	fillCrossword();
+	importCrosswordClues();
+	addClues();
 }
 
+
 // Import crossword clues into clueArray from JSON (INCOMPLETE)
-function importCrosswordClues(clueArray) {
+function importCrosswordClues() {
 	// Example clues to delete later
 	var temp = [
-		{"clueDirection":"across", "clueNumber":"1", "clueText":"His work is often framed", "clueLength":"6", "startPos":"(0,0)", "endPos":"(6,0)"},
-		{"clueDirection":"across", "clueNumber":"5", "clueText":"Its mother's first to lactate", "clueLength":"6", "startPos":"(9,0)", "endPos":"(14,0)"},
-		{"clueDirection":"across", "clueNumber":"9", "clueText":"Tall thin person training runners", "clueLength":"8", "startPos":"(0,2)", "endPos":"(7,2)"},
-		{"clueDirection":"across", "clueNumber":"10", "clueText":"Astronaut found in the local, drinking", "clueLength":"6", "startPos":"(9,2)", "endPos":"(14,2)"},
-		{"clueDirection":"across", "clueNumber":"17", "clueText":"Subdued sound of cattle", "clueLength":"3", "startPos":"(12,7)", "endPos":"(14,7)"},
-		{"clueDirection":"across", "clueNumber":"19", "clueText":"Dismissed when on strike", "clueLength":"3", "startPos":"(0,9)", "endPos":"(2,9)"},
-		{"clueDirection":"across", "clueNumber":"20", "clueText":"Contributory funds one put on daily account", "clueLength":"10", "startPos":"(5,9)", "endPos":"(14,9)"},
-		{"clueDirection":"across", "clueNumber":"26", "clueText":"Listener following close win", "clueLength":"6", "startPos":"(0,12)", "endPos":"(5,12)"},
-		{"clueDirection":"across", "clueNumber":"27", "clueText":"Furtive sort of hat style", "clueLength":"8", "startPos":"(7,12)", "endPos":"(14,12)"},
-		{"clueDirection":"across", "clueNumber":"28", "clueText":"Bearing a flower in March or April?", "clueLength":"6", "startPos":"(0,14)", "endPos":"(5,14)"},
-		{"clueDirection":"across", "clueNumber":"29", "clueText":"Girl with two articles to stow away safely", "clueLength":"7", "startPos":"(8,14)", "endPos":"(14,14)"},
-		{"clueDirection":"down", "clueNumber":"1", "clueText":"Jeer? I beg to differ!", "clueLength":"4", "startPos":"(0,0)", "endPos":"(0,3)"},
-		{"clueDirection":"down", "clueNumber":"2", "clueText":"Open with a shock", "clueLength":"4", "startPos":"(2,0)", "endPos":"(2,3)"},
-		{"clueDirection":"down", "clueNumber":"3", "clueText":"Understood devil comes to a lawful end", "clueLength":"8", "startPos":"(4,0)", "endPos":"(4,7)"},
-		{"clueDirection":"down", "clueNumber":"4", "clueText":"Australian football is supreme, OK?", "clueLength":"5", "startPos":"(6,0)", "endPos":"(6,4)"}
+		{"clueDirection":"across", "clueNumber":"1", "clueText":"Physician brings fish around", "clueLength":"3", "startPos":"(0,0)", "endPos":"(2,0)"},
+		{"clueDirection":"across", "clueNumber":"2", "clueText":"Times when things appear obscure?", "clueLength":"6", "startPos":"(0,1)", "endPos":"(5,1)"},
+		{"clueDirection":"across", "clueNumber":"3", "clueText":"Observe odd characters in scene", "clueLength":"3", "startPos":"(0,2)", "endPos":"(2,2)"},
+		{"clueDirection":"across", "clueNumber":"4", "clueText":"We surrounded strike snowy", "clueLength":"5", "startPos":"(0,3)", "endPos":"(4,3)"},
+		{"clueDirection":"across", "clueNumber":"5", "clueText":"Challenging sweetheart heartlessly", "clueLength":"6", "startPos":"(0,4)", "endPos":"(5,4)"},
+		{"clueDirection":"across", "clueNumber":"6", "clueText":"Armor in the post", "clueLength":"4", "startPos":"(0,5)", "endPos":"(3,5)"},
+		{"clueDirection":"across", "clueNumber":"7", "clueText":"His work is often framed", "clueLength":"6", "startPos":"(0,6)", "endPos":"(5,6)"},
+
+		{"clueDirection":"down", "clueNumber":"8", "clueText":"Jeer? I beg to differ!", "clueLength":"4", "startPos":"(14,0)", "endPos":"(14,3)"},
 	]; // DELETE LATER
 
 	// Importing temp array into clueArray
@@ -59,8 +83,8 @@ function importCrosswordClues(clueArray) {
 	}	
 }
 
-// Add clue information to left side of page (COMPLETE)
-function addClues(clueArray) {
+// Add clue information to left side of page and crossword (COMPLETE)
+function addClues() {
 	// Add clues to clue list
 	for (var i = 0; i < clueArray.length; i++) {
 		var table = document.getElementById(clueArray[i][0]+"Clues");
@@ -68,7 +92,6 @@ function addClues(clueArray) {
 		var row = table.insertRow();
 		var cell = row.insertCell(0);
 		//cell.innerHTML = "<img src='img/statusAdded.jpg' border=0/>";
-		cell.innerHTML = "<p> </p>";
 		clueArray[i][9] = cell;
 
 		var temp = "";
@@ -80,6 +103,16 @@ function addClues(clueArray) {
 		var clueText = clueArray[i][1] + ". " + clueArray[i][2] + " (" + clueArray[i][3] + ")";
 		cell = row.insertCell(0);
 		cell.innerHTML = clueText;
+
+		var xx = clueArray[i][4];
+		var yy = clueArray[i][5];
+		var num = clueArray[i][1];
+
+		document.getElementById("crosswordTable").rows[yy].cells[xx].innerHTML = '.<div class="daySubscript">' + num + '</div>';
+
+		// Log clue added
+		log("Added " + clueArray[i][0] + " Clue: " + clueArray[i][1] + ". " + clueArray[i][2] + " (" + clueArray[i][3] + ").");
+		log("");
 	}
 
 	// Unhighlight crossword table
@@ -98,8 +131,8 @@ function addClues(clueArray) {
 }
 
 
-//
-function generateHints(clueArray, answerArray) {
+// Generate hints by iterating through the answerArray (COMPLETE)
+function generateHints() {
 
 	for (var i = 0; i < clueArray.length; i++) {
 		var temp = "";
@@ -126,18 +159,22 @@ function generateHints(clueArray, answerArray) {
 
 
 // Use Edmund to find answers to clues (INCOMPLETE)
-function updateCrosswordByEdmund(clueArray, answerArray) {
+function updateCrosswordByEdmund() {
 	for (var i = 0; i < clueArray.length; i++) {
+		log("Sending " + clueArray[i][1] + " " + clueArray[i][0] + " to Edmund.");
 		clueArray[i][9].innerHTML = "<img src='img/statusCalculating.png' border=0/>";
-		// send clue to edmund, get response
-		var newAnswer = sentToEdmund(clueArray[i][2], clueArray[i][3], clueArray[i][10]);
 
-		if (!(newAnswer === "")) {
+		// send clue to edmund, get response
+		var newAnswer = sendToEdmund(clueArray[i][2], clueArray[i][3], clueArray[i][10]);
+
+		if (!(newAnswer === "*")) {
 			clueArray[i][10] = newAnswer;
 			updateAnswerArray(clueArray, answerArray, i);
 		}
 		else {
-			clueArray[clueIndexNumber][9].innerHTML = "<img src='img/statusFailed.png' border=0/>";
+			clueArray[i][9].innerHTML = "<img src='img/statusFailed.png' border=0/>";
+			log("Edmund could not resolve " + clueArray[i][1] + " " + clueArray[i][0] + ".");
+			log("");
 		}
 	}
 }
@@ -145,25 +182,33 @@ function updateCrosswordByEdmund(clueArray, answerArray) {
 
 // Send clue to Edmund with hint (INCOMPLETE)
 function sendToEdmund(clueText, clueLength, hint) {
-	var url = "http://localhost:18800/solve?clue=" + clueText + "&hint=" + hint + "&length=" + clueLength;
-	return "";
+	var url = "http://localhost:9090/solve?clue=" + clueText + "&hint=" + hint + "&length=" + clueLength;
+	alert(url);
+	
+	// ajax request
+	$.getJSON(url, function(result){
+		alert(result);
+	});
+
+	
+	return "*"; // EDMUND FAILURE
 }
 
 
 // Update answerArray with new answers (COMPLETE)
-function updateAnswerArray(clueArray, answerArray, clueIndexNumber, hint) {
+function updateAnswerArray(clueIndexNumber, hint) {
 	// fill answerArray with new answer, update status
 	if (clueArray[clueIndexNumber][0] === "across") {
 		var temp = 0;
 		for (var j = parseInt(clueArray[clueIndexNumber][4]); j < parseInt(clueArray[clueIndexNumber][6])+1; j++) {
-			updateCell(answerArray, j, clueArray[clueIndexNumber][5], hint.substring(temp, temp+1), clueArray);
+			updateCell(j, clueArray[clueIndexNumber][5], hint.substring(temp, temp+1));
 			temp++;
 		}
 	}
 	if (clueArray[clueIndexNumber][0] === "down") {
 		var temp = 0;
 		for (var j = parseInt(clueArray[clueIndexNumber][5]); j < parseInt(clueArray[clueIndexNumber][7])+1; j++) {
-			updateCell(answerArray, clueArray[clueIndexNumber][4], j, hint.substring(temp, temp+1), clueArray);
+			updateCell(clueArray[clueIndexNumber][4], j, hint.substring(temp, temp+1));
 			temp++;
 		}
 	}
@@ -173,7 +218,7 @@ function updateAnswerArray(clueArray, answerArray, clueIndexNumber, hint) {
 
 
 // Fill the crossword based on the answerArray (COMPLETE)
-function fillCrossword(answerArray) {
+function fillCrossword() {
 	for (var i = 0; i < 15; i++) {
 		for (var j = 0; j < 15; j++) {
 			document.getElementById("crosswordTable").rows[i].cells[j].innerHTML = answerArray[i][j];
@@ -190,10 +235,10 @@ function getCell(x, y) {
 
 
 // Update the cell contents at the given coordinates (COMPLETE)
-function updateCell(answerArray, x, y, content, clueArray) {
+function updateCell(x, y, content) {
 	answerArray[x][y] = content;
-	document.getElementById("crosswordTable").rows[y].cells[x].innerHTML = content.toString();
-	generateHints(clueArray, answerArray);
+	document.getElementById("crosswordTable").rows[y].cells[x].innerHTML = content.toString() + document.getElementById("crosswordTable").rows[y].cells[x].innerHTML.substring(1);
+	generateHints();
 }
 
 
@@ -206,4 +251,10 @@ function setBlank(x, y) {
 // Unblank the cell at the given coordinates (COMPLETE)
 function setUnblank(x, y) {
 	var cell = document.getElementById("crosswordTable").rows[parseInt(y)].cells[parseInt(x)].className = "crosswordCell";
+}
+
+// Log messages to the status pane (COMPLETE)
+function log(message) {
+	document.getElementById("statusPane").innerHTML = document.getElementById("statusPane").innerHTML + "&#10;" + message;
+	document.getElementById("statusPane").scrollTop = document.getElementById("statusPane").scrollHeight 
 }
