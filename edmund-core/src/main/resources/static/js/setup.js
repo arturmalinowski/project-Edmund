82f7efa76dc5aa81049f16891506c39c8948bd33
Damian Van Vuuren <vanvuurendamian@gmail.com>
2015-02-19 16:47:22 +0000

    Damian: Added status pane to web UI

--- edmund-core/src/main/resources/static/js/setup.js
+++ edmund-core/src/main/resources/static/js/setup.js
@@ -6,21 +6,4 @@ for (var i = 0; i < 15; i++) {
 	answerArray.push([".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."]);
 }
 
-setupTable();
-
-fillCrossword(answerArray);
-
-importCrosswordClues(clueArray);
-addClues(clueArray);
-//updateCrosswordByEdmund(clueArray, answerArray);
-
-// Tests to delete
-updateAnswerArray(clueArray, answerArray, "0", "1abcde"); //
-updateAnswerArray(clueArray, answerArray, "1", "5abcde"); //
-updateAnswerArray(clueArray, answerArray, "2", "9abcdefg"); //
-updateAnswerArray(clueArray, answerArray, "3", "10abcd"); //
-updateAnswerArray(clueArray, answerArray, "12", "2abcd"); //
-
-//generateHints(clueArray, answerArray);
-//alert(clueArray[0]);
-//alert("done");
+setupTable(clueArray, answerArray);
