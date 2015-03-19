749dbd6d0c0de34b99dd3dd5418afafa4464a64b
Damian Van Vuuren <vanvuurendamian@gmail.com>
2015-03-19 16:22:06 +0000

    Damian: Removed unnecesary files

--- edmund-core/src/main/resources/static/js/setup.js
+++
@@ -1,9 +0,0 @@
-// Arrays
-// [clueDirection, clueNumber, clueText, clueLength, startX, startY, endX, endY, status, statusLink, hint]
-var clueArray = [];
-var answerArray = [];
-for (var i = 0; i < 15; i++) {
-	answerArray.push([".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."]);
-}
-
-setupTable(clueArray, answerArray);
